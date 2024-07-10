a116ee3ad41bcfe86d281613aa11bd3e934be57f
Kenan Mammadli <kenan@doe.org>
2024-07-10 13:00:40 -0400

    added command line support for browser in Driver class

---
+++ crossbrowser.sh
@@ -0,0 +1,3 @@
+mvn test -D xmlFile=smoke-test-suite.xml -D browser=chrome-Headless
+mvn test -D xmlFile=smoke-test-suite.xml -D browser=Edge-headless
+mvn test -D xmlFile=smoke-test-suite.xml -D browser=FIrefox-HEADLESS
